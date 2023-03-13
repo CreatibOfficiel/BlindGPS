ff117a49050dce1c30540c17eca81762202c3b4e
BDanielo <daniel@armieux.fr>
2023-03-13 10:58:19 +0100

    Initial push

---
+++ app/src/main/java/com/example/projetv2/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.projetv2.ui;
+
+import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
