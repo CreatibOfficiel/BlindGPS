ba3cec95b649bcc8edef5ba3df09014b311f4c29
BDanielo <daniel@armieux.fr>
2023-03-15 11:28:31 +0100

    Last Localisation added
    Last localisation add, and some comments and tweaks

--- app/src/main/java/com/example/blindGPS/ui/AppActivity.java
+++ app/src/main/java/com/example/blindGPS/ui/AppActivity.java
@@ -1,14 +1,23 @@
 package com.example.blindGPS.ui;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.blindGPS.R;
+import com.example.blindGPS.model.GPX;
 import com.example.blindGPS.pdr.DeviceAttitudeHandler;
 import com.example.blindGPS.pdr.StepDetectionHandler;
 import com.example.blindGPS.pdr.StepPositioningHandler;
@@ -18,9 +27,18 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    // declaration des variables
+    private GPX gpx;
     private GoogleMapTracer mGoogleMapTracer;
     private GoogleMap mMap;
     private StepPositioningHandler mStepPositioningHandler;
@@ -28,17 +46,32 @@ public class AppActivity extends FragmentActivity implements OnMapReadyCallback 
     private SensorManager sensorManager;
     private StepDetectionHandler stepDetectionHandler;
     private DeviceAttitudeHandler deviceAttitudeHandler;
+
+    // definition de la taille du pas
     private final float TAILLEPAS = (float) 0.4;
-    private boolean tracing = false; // une trace est en cours ou non
+    private boolean tracing = false; // status du tracé (en cours ou non)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        // On initialise le fichier .gpx
+        gpx = new GPX();
+        // On essaie d'ouvrir le fichier .gpx
+        try {
+            InputStream is = getAssets().open("simple.gpx");
+            gpx = GPX.parse(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //  on ajoute le fragment de la carte
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // Récupération de la taille de l'écran
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         stepDetectionHandler = new StepDetectionHandler(sensorManager);
         deviceAttitudeHandler = new DeviceAttitudeHandler(sensorManager);
@@ -47,15 +80,18 @@ public class AppActivity extends FragmentActivity implements OnMapReadyCallback 
 
     @Override
     protected void onResume() {
+        // on reprend la détection de pas et de l'orientation
         super.onResume();
         stepDetectionHandler.start();
         deviceAttitudeHandler.start();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        stepDetectionHandler.stop();
-        deviceAttitudeHandler.stop();
+        // on ne met pas en pause les capteurs lors de la veille de l'appareil, sinon ne perd la localisation
+        //        stepDetectionHandler.stop();
+        //        deviceAttitudeHandler.stop();
     }
 
     private StepDetectionHandler.StepDetectionListener stepListener = new StepDetectionHandler.StepDetectionListener() {
@@ -64,41 +100,48 @@ public class AppActivity extends FragmentActivity implements OnMapReadyCallback 
         public void onNewStepDetected() {
             double bearing = deviceAttitudeHandler.getBearing();
             LatLng newPos = mStepPositioningHandler.computeNextStep(TAILLEPAS, (float) bearing);
-            if(mMap != null && tracing)
+            if (mMap != null && tracing)
                 mGoogleMapTracer.newPoint(newPos);
         }
     };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        // on récupère la map
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID); // vu satelitte de la map
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(20)); // on zoom pour un meilleur confort au départ
 
-        // centre la map sur un point dans Grenoble (IUT1)
-        // 45.19275317406673, 5.7176893570083545
+        // on met la map en mode satelite
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        askLocalisationPermission();
+
+        // on se place sur l'IUT 1
         LatLng grenoble = new LatLng(45.19275317406673, 5.7176893570083545);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(grenoble));
 
+        SetLocalisation(grenoble);
+
+        // on instancie le tracer de map
         mGoogleMapTracer = new GoogleMapTracer(mMap, deviceAttitudeHandler);
-        mStepPositioningHandler = new StepPositioningHandler(null); // on défini une position initiale null pour ne pas avoir de point/marker avant un click de l'user
+
+        // on instancie le gestionnaire de positionnement
+        mStepPositioningHandler = new StepPositioningHandler(null);
 
         mOnMapClickListener = new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) { // au clic sur la map
-                // si une trace est en cours, on la stop, on ajoute un drapeau de fin et on affiche une bulle d'information
-                if(tracing){
+                // si il y a deja une trace en cours, on la termine
+                if (tracing) {
                     tracing = false;
                     mGoogleMapTracer.endSegment();
-                    Toast toast = Toast.makeText(AppActivity.this, "Fin du segment", Toast.LENGTH_SHORT);
+                    Toast toast = Toast.makeText(AppActivity.this, "Fin de la trace", Toast.LENGTH_SHORT);
                     toast.show();
-                }else{
-                    // si aucune trace n'est en cours de création, on la démarre avec un drapeau de départ, un nouveau point et un message d'information
+                } else {
+                    // sinon on en démarre une nouvelle
                     tracing = true;
-                    if(mStepPositioningHandler.getmCurrentPosition() == null){
+                    if (mStepPositioningHandler.getmCurrentPosition() == null) {
                         mStepPositioningHandler.setmCurrentPosition(latLng);
                     }
-                    Toast toast = Toast.makeText(AppActivity.this, "Début du segment", Toast.LENGTH_SHORT);
+                    Toast toast = Toast.makeText(AppActivity.this, "Début de la trace", Toast.LENGTH_SHORT);
                     toast.show();
                     mGoogleMapTracer.newSegment();
                     mGoogleMapTracer.newPoint(mStepPositioningHandler.getmCurrentPosition());
@@ -108,11 +151,93 @@ public class AppActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap.setOnMapClickListener(mOnMapClickListener);
     }
 
-    // au clic sur le bouton d'export en haut à droite de la map on lance la fonction de création du fichier et de téléchargement, puis on affiche une bulle d'infotmaition
-    public void export(View view){
-        mGoogleMapTracer.exportToXML("tracks2.gpx");
+    //  fonction d'export d'un fichier gpx selon le tracé de l'utilisateur
+    public void export(View view) {
+        //  création du fichier gpx
+        mGoogleMapTracer.exportToXML("tracks.gpx");
 
-        Toast toast = Toast.makeText(AppActivity.this, "Fichier exporté dans les téléchargement", Toast.LENGTH_SHORT);
+        //  affichage d'un message de confirmation de la sauvegarde du fichier gpx
+        Toast toast = Toast.makeText(AppActivity.this, "Exportation faite dans le dossier téléchargement", Toast.LENGTH_SHORT);
         toast.show();
     }
+
+    // fonction d'affichage du tracer d'un fichier gpx
+    public void showTrack(View view) {
+        //  parcour du fichier gpx
+        for (GPX.Track t : gpx) {
+            // on instancie un polyline pour afficher la trace
+            Polyline line = mMap.addPolyline(new PolylineOptions());
+
+            //  ainsi qu'un tableau de LatLng pour récupérer les points de la trace
+            List<LatLng> latLngList = new ArrayList<LatLng>();
+
+            // on parcourt les traces pour récupérer les points
+            for (GPX.TrackSegment ts : t) {
+                for (GPX.TrackPoint tp : ts) {
+                    // on ajoute les points à la liste
+                    latLngList.add(tp.position);
+                }
+            }
+
+            // on ajoute les points au polyline
+            line.setPoints(latLngList);
+            // avec une epaisseur
+            line.setWidth(5);
+            // et une couleur
+            line.setColor(Color.RED);
+        }
+
+        // on recupère la taille de l'écran
+        Display d = getWindowManager().getDefaultDisplay();
+        DisplayMetrics m = new DisplayMetrics();
+        d.getMetrics(m);
+
+        // pour ensuite centrer la map sur la trace
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(gpx.getLatLngBounds(), m.widthPixels, m.heightPixels, 30));
+    }
+
+    public void localisation(View view) {
+        SetLocalisation(GetLocalisation());
+    }
+
+    public void SetLocalisation(LatLng position) {
+
+        // on centre la map sur la dernière localisation connue
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+
+        // on zoom sur la dernière localisation connue
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(20));
+    }
+
+    public LatLng GetLocalisation() {
+        // on récupère le service de localisation
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // on récupère la dernière localisation connue
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        // point de test de l'IUT 1
+        LatLng grenoble = new LatLng(45.19275317406673, 5.7176893570083545);
+
+        LatLng derniereLocalisation = grenoble;
+
+        //  on vérifie que la localisation n'est pas null
+        if (location != null) {
+            // on crée un LatLng avec les coordonnées de la localisation
+            derniereLocalisation = new LatLng(location.getLatitude(), location.getLongitude());
+        }
+
+        return derniereLocalisation;
+    }
+
+    public void askLocalisationPermission () {
+        // on vérifie que l'on a bien les permissions pour accéder à la localisation
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // si on n'a pas les permissions, on les demande
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+
+    }
+
 }
